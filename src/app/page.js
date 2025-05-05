ae04c0c2e1d3a3c4413cd0255b059d0c7301361f
Atish Kumar Sahu <kumarsahuatishoff280301@gmail.com>
2025-05-05 17:34:18 +0530

    Final Commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import WelcomePage from "@/components/pages/welcome";
+
+export default function Home () {
+    return (
+        <>
+            <h1 className="poppins-regular-H1">HOME</h1>
+            <WelcomePage />
+        </>
+    );
+}
